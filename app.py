fc485d3c9220dabef6cd808f06c43ace9025f344
amitgandhinz <amit@gandhi.co.nz>
2014-07-12 11:03:39 -0400

    converted to a flask app

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template
+
+# initialization
+app = Flask(__name__)
+app.config.update(
+    DEBUG=True,
+)
+
+
+# controllers
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+# launch
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
